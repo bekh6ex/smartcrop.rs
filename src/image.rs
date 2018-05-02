f2b8b1563ade63551e6d10d991b04a91ef983b8f
Aleksey Bekh-Ivanov <6ex@mail.ru>
2018-05-02 22:11:17 +0200

    Implement `Image` for `image::GenericImage`

--- src/image.rs
+++ src/image.rs
@@ -1,11 +1,15 @@
 extern crate image as image_ext;
 
-use self::image_ext::{DynamicImage, FilterType, GenericImage};
+use self::image_ext::{FilterType, GenericImage, Pixel, imageops};
 use RGB;
 use Image;
+use std;
 
-impl Image for DynamicImage {
-    type ResizeToImage = Self;
+impl<I, P> Image for I
+    where I: GenericImage<Pixel=P> + Clone + 'static,
+          P: Pixel<Subpixel=u8> + 'static {
+    type ResizeToImage = self::image_ext::ImageBuffer<P, std::vec::Vec<u8>>;
+
 
     fn width(&self) -> u32 {
         GenericImage::width(self)
@@ -15,17 +19,15 @@ impl Image for DynamicImage {
         GenericImage::height(self)
     }
 
-    fn resize(&self, width: u32) -> Self {
-        if width == GenericImage::width(self) {
-            return self.clone();
-        }
-        let height = GenericImage::height(self);
+    fn resize(&self, width: u32) -> Self::ResizeToImage {
+        let height = (width as f64 / GenericImage::width(self) as f64 * GenericImage::height(self) as f64).round() as u32;
 
-        self.resize(width, height, FilterType::Lanczos3)
+        imageops::resize(self, width, height, FilterType::Lanczos3)
     }
 
     fn get(&self, x: u32, y: u32) -> RGB {
-        let px = self.get_pixel(x, y);
+        let px = self.get_pixel(x, y).to_rgb();
+
         let r = px[0];
         let g = px[1];
         let b = px[2];
